c09c3965df7cc905f74a1a510b9d921d039e8a29
Robin Bécherraz <robinbecherraz2@hotmail.fr>
2025-11-21 17:28:32 +0100

    geodetective

--- src/_includes/components.js
+++ src/_includes/components.js
@@ -4,7 +4,7 @@ export default function(eleventyConfig) {
     eleventyConfig.addPairedShortcode("box_1", function(content, title = "") {
         const titleHtml = title ? `<h2 class="w-fit font-mono -mt-[0.75rem] -ml-[2px] bg-[#030303] pt-[2px] pb-[4px] pr-[7px]">${title}</h2>` : '';
         return `
-        <div class="box h-fit max-w-[25rem] border border-gray rounded-md bg-[#030303]">
+        <div class="box w-fit h-fit max-w-[25rem] border border-gray rounded-md bg-[#030303]">
             ${titleHtml}
             <div class="markdown-wrapper max-h-[35rem] overflow-y-auto p-3 text-[0.875rem]">
                 ${content}
---
+++ src/geodetective/Geodetective_rules.md
@@ -0,0 +1,9 @@
+* The goal is to find the GPS location of an image
+* You can use **any tools** you want
+  * search by image
+  * metadata
+  * ask your friends
+  * personnal knowledge
+  * OSINT
+  * ...
+* Voilà
